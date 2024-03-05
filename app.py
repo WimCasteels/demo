7955c00d4fe9bd95dbf3ecf38536c77b5a4eca13
WimCasteels <wim.casteels@ap.be>
2024-03-05 11:05:56 +0100

    remove TTS and STT

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_mic_recorder import speech_to_text
 from openai import OpenAI
 import os
-import sounddevice as sd 
+#import sounddevice as sd 
 import soundfile as sf
 
 state=st.session_state
@@ -60,36 +60,36 @@ if prompt := st.chat_input("Hoe kan ik je helpen?"):
 
 
 
-if text := speech_to_text(
-    language='nl',
-    start_prompt="Opname",
-    stop_prompt="Stop opname",
-    use_container_width=False,just_once=True,key='STT'):
-    with st.chat_message("user"):
-        st.markdown(text)
-    st.session_state.messages.append({"role": "user", "content": text})
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        #response = st.write_stream(response_generator())
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[{"role": "system", "content": st.session_state.system_prompt}] +
-            [{"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-        #print(stream.choices[0].message.content)
-    # TTS
-        speech = client.audio.speech.create(
-            model="tts-1",
-            voice="nova",
-            input=response)
-        file_path = os.getcwd() +"\\soundfile.mp3"
-        speech.stream_to_file(file_path)
-        audio_data, sample_rate = sf.read(file_path)
-        sd.play(audio_data, sample_rate)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+# if text := speech_to_text(
+#     language='nl',
+#     start_prompt="Opname",
+#     stop_prompt="Stop opname",
+#     use_container_width=False,just_once=True,key='STT'):
+#     with st.chat_message("user"):
+#         st.markdown(text)
+#     st.session_state.messages.append({"role": "user", "content": text})
+#     # Display assistant response in chat message container
+#     with st.chat_message("assistant"):
+#         #response = st.write_stream(response_generator())
+#         stream = client.chat.completions.create(
+#             model=st.session_state["openai_model"],
+#             messages=[{"role": "system", "content": st.session_state.system_prompt}] +
+#             [{"role": m["role"], "content": m["content"]}
+#                 for m in st.session_state.messages],
+#             stream=True,
+#         )
+#         response = st.write_stream(stream)
+#         #print(stream.choices[0].message.content)
+#     # TTS
+#         speech = client.audio.speech.create(
+#             model="tts-1",
+#             voice="nova",
+#             input=response)
+#         file_path = os.getcwd() +"\\soundfile.mp3"
+#         speech.stream_to_file(file_path)
+#         audio_data, sample_rate = sf.read(file_path)
+#         sd.play(audio_data, sample_rate)
+#     # Add assistant response to chat history
+#     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
